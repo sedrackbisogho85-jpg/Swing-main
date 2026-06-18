bc0159fb5ed75e079b902a00a2dd5eee6e55ddea
BWAMBALE BISOGHO SEDRACK <sedrackbisogho85@gmail.com>
2026-06-18 08:42:49 +0300

    new changes

--- jobconnect-frontend-main/src/components/AlertMessage.jsx
+++
@@ -1,7 +0,0 @@
-const AlertMessage = ({ type = 'danger', message }) => {
-  // Returning null means React renders nothing when there is no message.
-  if (!message) return null;
-  return <div className={`alert alert-${type} py-2`}>{message}</div>;
-};
-
-export default AlertMessage;
